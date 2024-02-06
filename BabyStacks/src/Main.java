2a35107950c9dccfb1a7ebe30ecb972270beaedd
Devin Ford <devin.ford@selu.edu>
2024-02-06 15:09:13 -0600

    fix

--- BabyStacks/src/Main.java
+++ BabyStacks/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         String b = "aet nomel";
         String c = "snoino dna revil etah I";
         String d = "SELUR LOBOC";
-        StringBuilder temp = new StringBuilder();
+        String temp = "";
 
         int i;
         Stack stack = new Stack();
@@ -15,37 +15,37 @@ public class Main {
         }
 
         while(!stack.isEmpty()){
-            temp.append(stack.pop());
+            temp = temp + stack.pop();
         }
         a = temp.toString();
-        temp = new StringBuilder();
+        temp = "";
 
         for(i = 0; i<b.length();i++){
             stack.push(b.charAt(i));
         }
 
         while(!stack.isEmpty()){
-            temp.append(stack.pop());
+            temp = temp + stack.pop();
         }
         b = temp.toString();
-        temp = new StringBuilder();
+        temp = "";
 
         for(i = 0; i<c.length();i++){
             stack.push(c.charAt(i));
         }
 
         while(!stack.isEmpty()){
-            temp.append(stack.pop());
+            temp = temp + stack.pop();
         }
         c = temp.toString();
-        temp = new StringBuilder();
+        temp = "";
 
         for(i = 0; i<d.length();i++){
             stack.push(d.charAt(i));
         }
 
         while(!stack.isEmpty()){
-            temp.append(stack.pop());
+            temp = temp + stack.pop();
         }
         d = temp.toString();
 
